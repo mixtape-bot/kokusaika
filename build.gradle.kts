1acfa24f8e93a8c45078831389cd1f3ee64d7160
2D <gino@dimensional.fun>
2021-05-25 05:44:07 -0700

    :wrench: jfrog?

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
   `maven-publish`
 
-  kotlin("jvm") version "1.5.0"
+  kotlin("jvm") version "1.5.10"
   kotlin("plugin.serialization") version "1.5.0"
 }
 
 group = "gg.mixtape"
 version = "1.0.0"
+val archivesBaseName = "kokusaika"
 
+/* dependencies */
 repositories {
   mavenCentral()
 }
@@ -27,7 +29,51 @@ dependencies {
   api("org.slf4j:slf4j-api:1.7.30")
 }
 
+/* jfrog bullshit */
+val sourcesJar = task<Jar>("sourcesJar") {
+  archiveClassifier.set("sources")
+  from(sourceSets["main"].allJava)
+}
+
+publishing {
+  repositories {
+    maven {
+      name = "jfrog"
+      url = uri("https://dimensional.jfrog.io/artifactory/maven")
+      credentials {
+        username = System.getenv("JFROG_USERNAME")
+        password = System.getenv("JFROG_PASSWORD")
+      }
+    }
+  }
+
+  publications {
+    create<MavenPublication>("jfrog") {
+      from(components["java"])
+
+      group = project.group as String
+      version = project.version as String
+      artifactId = archivesBaseName
+
+      artifact(sourcesJar)
+    }
+  }
+}
+
+tasks.build {
+  dependsOn(tasks.jar)
+  dependsOn(sourcesJar)
+}
+
+tasks.publish {
+  dependsOn(tasks.build)
+  onlyIf {
+    System.getenv("JFROG_USERNAME") != null && System.getenv("JFROG_PASSWORD") != null
+  }
+}
+
+/* kotlin compile */
 tasks.withType<KotlinCompile> {
-  kotlinOptions.jvmTarget = "13"
+  kotlinOptions.jvmTarget = "16"
   kotlinOptions.freeCompilerArgs = listOf("-Xopt-in=kotlin.RequiresOptIn")
 }
